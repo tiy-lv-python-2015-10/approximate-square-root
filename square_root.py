968f454b4b69ff523c4004ea3d0a0088670dbbda
Some Guy <abc@abc.com>
2015-10-05 23:32:49 -0700

    Added comments

--- square_root.py
+++ square_root.py
@@ -2,6 +2,8 @@ keep_going = True
 guess = 1
 max_error = 0.01
 
+
+#checks user input for a positive number
 while keep_going:
     try:
         num = int(input("Enter a positive number > "))
@@ -12,16 +14,20 @@ while keep_going:
     except:
         print("Pleass enter a positive number > ")
 
+# when root +- .01 away from 0, guess == the square root (approximately)
 root = (num - guess ** 2)
 count = 1
 
+# while root +- .01 away from 0, run loop until it is
 while abs(root) >= max_error:
     print("Guess #{} : {}".format(count, guess))
     guess = (guess + num / guess) / 2
     root = (num - guess ** 2)
     count += 1
 
+#prints each guess and what number guess it is
 print("Guess #{}: {}".format(count, guess))
 
+#rounds to 2 decimal places and prints 
 rounded = round(guess, 2)
 print("The rounded square root of {} is {}".format(num, rounded))
