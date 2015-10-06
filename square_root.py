54232be232152b31a79760e78057592427f71a60
Adam Helzer <vanhelzer@icloud.com>
2015-10-05 18:47:30 -0700

    easy square-root home work

--- square_root.py
+++ square_root.py
@@ -7,7 +7,7 @@ while running:
     best_guess = abs(user_number - guess**2)
     if best_guess <= 0.01:
         running = False
-        print("You have found apprx. square-root: ", best_guess)
-        
+        rounded = round(best_guess, 2)
+        print("You have found apprx. square-root: ", rounded)
     else:
          guess = (guess + user_number/guess) / 2
